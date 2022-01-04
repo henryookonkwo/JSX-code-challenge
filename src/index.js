540052848245a33b200a219e9da14c7e4388b815
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2022-01-04 16:04:11 -0500

    Add all answers to questions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
 //It should display a h1 heading.
+ReactDOM.render(
+  <div>
+    <h1>Todo list</h1>
+    <ul>
+      <li>Buy food</li>
+      <li>Cook Food</li>
+      <li>Eat food</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 //It should display an unordered list (bullet points).
+
 //It should contain 3 list elements.
