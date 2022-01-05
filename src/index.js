da5cdd58b1a4bde7eff4aebf58b0da32c4604d2c
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2022-01-04 20:41:30 -0500

    Add random images, remove UL

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img1 = "https://picsum.photos/200";
+const img2 = "https://picsum.photos/200";
+
+const randomNum = Math.floor(Math.random() * 200);
+const img3 = "https://picsum.photos/id/" + randomNum + "/200";
+
 //It should display a h1 heading.
 ReactDOM.render(
   <div>
-    <h1>Todo list</h1>
-    <ul>
-      <li>Buy food</li>
-      <li>Cook Food</li>
-      <li>Eat food</li>
-    </ul>
+    <h1 className="heading">Beautiful pictures</h1>
+    <div>
+      <img alt="random-grayscale" src={img1 + "?grayscale"} /> <br />
+      <img alt="random" src={img2} /> <br />
+      <img alt="random" src={img3} />
+    </div>
   </div>,
   document.getElementById("root")
 );
